9c45238bc9881dd5f95bcf53c3560b3404ce8e09
Ziqi Wang <ziqi.wang39@gmail.com>
2021-09-23 10:25:42 +1000

    Make the nav responsive

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,8 +1,15 @@
-import React from "react";
-import { Anchor } from "antd";
+import React, { useState } from "react";
+import { Anchor, Drawer, Button } from "antd";
 
 const { Link } = Anchor;
 const AppHeader = () => {
+  const [visible, setVisible] = useState(false);
+  const showDrawer = () => {
+    setVisible(true);
+  };
+  const onClose = () => {
+    setVisible(false);
+  };
   return (
     <div className="container-fluid">
       <div className="header">
@@ -10,15 +17,38 @@ const AppHeader = () => {
           <i className="fas fa-bolt"></i>
           <a href="http://www.google.com">Tech</a>
         </div>
-        <Anchor targetOffset={46}>
-          <Link href="#hero" title="HOME" />
-          <Link href="#about" title="ABOUT US" />
-          <Link href="#feature" title="FEATURES" />
-          <Link href="#works" title="HOW IT WORKS" />
-          <Link href="#faq" title="FAQ" />
-          <Link href="#pricing" title="PLANS" />
-          <Link href="#conact" title="CONTACT US" />
-        </Anchor>
+        <div className="mobileHidden">
+          <Anchor targetOffset={46}>
+            <Link href="#hero" title="HOME" />
+            <Link href="#about" title="ABOUT US" />
+            <Link href="#feature" title="FEATURES" />
+            <Link href="#works" title="HOW IT WORKS" />
+            <Link href="#faq" title="FAQ" />
+            <Link href="#pricing" title="PLANS" />
+            <Link href="#contact" title="CONTACT US" />
+          </Anchor>
+        </div>
+        <div className="mobileVisible">
+          <Button type="primary" onClick={showDrawer}>
+            <i className="fas fa-bars"></i>
+          </Button>
+          <Drawer
+            title="menu"
+            placement="right"
+            onClose={onClose}
+            visible={visible}
+          >
+            <Anchor targetOffset={46}>
+              <Link href="#hero" title="HOME" />
+              <Link href="#about" title="ABOUT US" />
+              <Link href="#feature" title="FEATURES" />
+              <Link href="#works" title="HOW IT WORKS" />
+              <Link href="#faq" title="FAQ" />
+              <Link href="#pricing" title="PLANS" />
+              <Link href="#contact" title="CONTACT US" />
+            </Anchor>
+          </Drawer>
+        </div>
       </div>
     </div>
   );
